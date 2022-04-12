d83af57690dd0490f3db3c2a06677b63f7f5e4a1
SydVic <fabrizio.bocca27@gmail.com>
2022-04-12 14:23:35 +0200

    creato script gioco dadi

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,5 +1,15 @@
 // generare due numeri random, uno per user e uno per computer
+const userNumber = Math.floor(Math.random() * 6) + 1;
+console.log("Il tuo numero", userNumber);
 
-// confrontare i due numeri e stabilire il vincitorre in base al numero più alto
+const computerNumber = Math.floor(Math.random() * 6) + 1;
+console.log("Il numero del pc", computerNumber);
 
-// comunicare l'esito del gioco
+// confrontare i due numeri e stabilire il vincitorre in base al numero più alto e comunicare l'esito del gioco
+if (userNumber > computerNumber) {
+    console.log("Hai vinto tu!")
+} else if (userNumber < computerNumber) {
+    console.log("Mi dispiace, ha vinto il pc!")
+} else if (userNumber == computerNumber) {
+    console.log("E' un pareggio!")
+}
