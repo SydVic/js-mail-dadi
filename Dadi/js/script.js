f2ab459cedc7f9e89d16b4da750a55afe2f3e3c3
SydVic <fabrizio.bocca27@gmail.com>
2022-04-12 14:26:49 +0200

    aggiunti punti e virgola dimenticati

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,15 +1,15 @@
 // generare due numeri random, uno per user e uno per computer
 const userNumber = Math.floor(Math.random() * 6) + 1;
-console.log("Il tuo numero", userNumber);
+console.log("Il tuo numero è", userNumber);
 
 const computerNumber = Math.floor(Math.random() * 6) + 1;
-console.log("Il numero del pc", computerNumber);
+console.log("Il numero del pc è", computerNumber);
 
 // confrontare i due numeri e stabilire il vincitorre in base al numero più alto e comunicare l'esito del gioco
 if (userNumber > computerNumber) {
-    console.log("Hai vinto tu!")
+    console.log("Hai vinto tu!");
 } else if (userNumber < computerNumber) {
-    console.log("Mi dispiace, ha vinto il pc!")
+    console.log("Mi dispiace, ha vinto il pc!");
 } else if (userNumber == computerNumber) {
-    console.log("E' un pareggio!")
+    console.log("E' un pareggio!");
 }
