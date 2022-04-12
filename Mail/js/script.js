054d8bbbc78aacd555979ec80a945674b6ab66f2
SydVic <fabrizio.bocca27@gmail.com>
2022-04-12 14:04:21 +0200

    creato script per controllo mail inserita da utente

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,5 +1,25 @@
 // chiedo la mail all'utente
+const userMail = prompt("Ciao! Qual'è la tua mail?");
+console.log(userMail);
 
 // controllo che sia presente nella lista delle mail autorizzate all'accesso
+// array per lista mail autorizzate all'accesso
+const mailList = ["pincopallo@gmail.com", "pippo@yahoo.com", "topolino@libero.it", "tiziocaio@gmail.com", "sempronio@protonmail.com"];
+
+let authorizedMail = false;
+
+// scorro le mail della lista per vedere se quella immessa è presente
+for (let i = 0; i < mailList.length; i++) {
+    const mailCheck = mailList[i];
+    if (mailCheck === userMail) {
+        authorizedMail = true;
+    }
+    console.log(authorizedMail);
+}
 
 // messaggio sull'esito del controllo
+if (authorizedMail === true) {
+    console.log("La tua mail è presente tra quelle autorizzate all'accesso");
+} else {
+    console.log("Mi dispiace, la tua mail non è presente tra quelle autorizzate all'accesso")
+}
